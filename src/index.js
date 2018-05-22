e2588393f756603b88bb9c5486e4d47c4ca49956
cgolinski <cgolinski@users.noreply.github.com>
2018-05-21 21:38:23 -0400

    adds redux-logger, configs redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
+import { Router, Route, browserHistory } from "react-router";
+import { syncHistoryWithStore } from "react-router-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import rootReducer from "./redux/reducers";
+import "./index.css";
+import App from "./components/App";
+import registerServiceWorker from "./registerServiceWorker";
+
+const loggerMiddleware = createLogger();
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(loggerMiddleware)
+);
+
+const history = syncHistoryWithStore(browserHistory, store);
+
+const ReduxApp = () => (
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App} />
+    </Router>
+  </Provider>
+);
+
+ReactDOM.render(<ReduxApp />, document.getElementById("root"));
 registerServiceWorker();
